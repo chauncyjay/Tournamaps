d5327c187bb457d720f614aef14d2afa9f3466a9
CJ <cj@insrcd.com>
2016-10-21 09:50:06 -0400

    adding tablesorting and beginning of bracket demo

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
-
-  // Get started!
+    
+    //Use this to make tables added dynamically sortable
+    //Documentation: http://www.kryogenix.org/code/browser/sorttable/
+    //sorttable.makeSortable(newTableObject);
 
 });
