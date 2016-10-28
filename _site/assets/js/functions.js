b4faafb06d7ca707e23df5f6e561348821b97fc8
CJ <cj@insrcd.com>
2016-10-27 22:22:25 -0400

    javascript moved around for bracket. bonus logo added

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,11 +1,76 @@
 var $, console; //STUPID JSLINT STUFF
-var EVENT_URL = 'bracket-example-1.html',
+var BRACKET_URL = 'bracket.html',
     MAP_URL = 'map.html',
     STATUS_URL = '',
     PRIZED_URL = '',
     BRACKET_LIST_HTML = 'bracketlist.html',
     i;
 
+$(document).ready(function () {
+    
+    //Use this to make tables added dynamically sortable
+    //Documentation: http://www.kryogenix.org/code/browser/sorttable/
+    //sorttable.makeSortable(newTableObject);
+    
+    if ($(location).attr('pathname') === '/' + BRACKET_LIST_HTML) {  //only runs on bracket list 
+        buildBracketList();
+    }
+    if ($(location).attr('pathname') === '/' + BRACKET_URL) {  //only runs on bracket list 
+        buildBracket();
+    }
+});
+function buildBracket() {
+    var saveData = {
+        teams:
+            getBracket(),
+        results: [
+            [
+                [[0, 0], [0, 0], [0, 0], [0, 0]],
+                [[0, 0], [0, 0]],
+                [[0, 0], [0, 0]]
+            ]
+        ]
+    };
+
+    $(function() {
+        var container = $('div#singlebracket');
+        container.bracket({
+            init: saveData,
+            save: saveFn,
+            userData: "http://myapi"
+        });
+
+        /* You can also inquiry the current data */
+        var data = container.bracket('data');
+        $('#dataOutput').text(jQuery.toJSON(data));
+    })
+}
+function buildBracketList() {
+    //builds each row by getting information for the table
+    for (i = 1; i < 4; i += 1) {
+
+        //create string and add tags and name
+        var tableString = '<tr><td><div onclick="location.href=\'' + BRACKET_URL + '\'\">' + getEvent(i) + '</div></td>';
+
+        //add more tags and location
+        tableString += '<td><div onclick=\"location.href=\'' + MAP_URL + '\'\">' + getLocation(i) + '</div></td>';
+
+        //add more tags and status
+        tableString += '<td><div onclick=\"location.href=\'' + STATUS_URL + '\'\">' + getStatus(i) + '</div></td>';
+
+        tableString += '<td><div onclick=\"location.href=\'' + PRIZED_URL + '\'\">' + getPrized(i) + '</div></td></tr>';
+
+        //console.log(tableString);
+
+        $('#brackets').find('tbody:last').append(tableString);
+    }
+}
+function getBracket(){
+    return [["Player 1", "Player 8"],
+          ["Player 4", "Player 5"],
+          ["Player 3", "Player 6"],
+          ["Player 2", "Player 7"]];
+}
 function getEvent(num) {
     return 'Event ' + num;
 }
@@ -26,29 +91,18 @@ function getPrized(num) {
         return 'Complete';
     }
 }
-$(document).ready(function () {
-    
-    //Use this to make tables added dynamically sortable
-    //Documentation: http://www.kryogenix.org/code/browser/sorttable/
-    //sorttable.makeSortable(newTableObject);
-    
-    if ($(location).attr('pathname') === '/' + BRACKET_LIST_HTML) {  //only runs on bracket list 
-        for (i = 1; i < 4; i += 1) {
-
-            //create string and add tags and name
-            var tableString = '<tr><td><div onclick="location.href=\'' + EVENT_URL + '\'\">' + getEvent(i) + '</div></td>';
-
-            //add more tags and location
-            tableString += '<td><div onclick=\"location.href=\'' + MAP_URL + '\'\">' + getLocation(i) + '</div></td>';
-
-            //add more tags and status
-            tableString += '<td><div onclick=\"location.href=\'' + STATUS_URL + '\'\">' + getStatus(i) + '</div></td>';
-
-            tableString += '<td><div onclick=\"location.href=\'' + PRIZED_URL + '\'\">' + getPrized(i) + '</div></td></tr>';
-
-            //console.log(tableString);
-
-            $('#brackets').find('tbody:last').append(tableString);
-        }
-    }
-});
+/* Called whenever bracket is modified
+*
+* data:     changed bracket object in format given to init
+* userData: optional data given when bracket is created.
+*/
+function saveFn(data, userData) {
+    var json = jQuery.toJSON(data)
+    $('#saveOutput').text('POST '+userData+' '+json)
+    /* You probably want to do something like this
+    jQuery.ajax("rest/"+userData, {contentType: 'application/json',
+                                  dataType: 'json',
+                                  type: 'post',
+                                  data: json})
+    */
+}
